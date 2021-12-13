e49d00a9b23889773569d7b290cea765bf5bcd73
韩旭小天才 <905583741@qq.com>
2021-12-13 17:21:29 +0800

    fix:调整cssModule

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
 /*
  * @Author: your name
  * @Date: 2021-12-13 14:52:54
- * @LastEditTime: 2021-12-13 16:24:17
+ * @LastEditTime: 2021-12-13 17:18:16
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /hxreact/src/index.tsx
  */
 import * as React from 'react'
-import styles from './styles.module.css'
+import './styles.css'
 import Button from './packages/Button'
 interface Props {
   text: string
 }
 
 const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return <div className='test'>Example Component: {text}</div>
 }
 
 export { ExampleComponent, Button }
