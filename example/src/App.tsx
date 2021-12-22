4ede3518a6623d3e1b3000a3266846f1b02be580
韩旭小天才 <905583741@qq.com>
2021-12-22 16:39:41 +0800

    feat:完成 Row、Col 布局组件

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-13 14:52:54
- * @LastEditTime: 2021-12-21 16:53:40
+ * @LastEditTime: 2021-12-22 16:39:32
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /hxreact/example/src/App.tsx
@@ -28,80 +28,6 @@ const App = () => {
         Click me!
       </Button>
       <Icon name={'icon-Color-fill'} />
-
-      {/* <Row>
-        <Col span='24'>
-          <div className='grid-content bg-purple-dark'></div>
-        </Col>
-      </Row>
-      <Row>
-        <Col span='12'>
-          <div className='grid-content bg-purple'></div>
-        </Col>
-        <Col span='12'>
-          <div className='grid-content bg-purple-light'></div>
-        </Col>
-      </Row>
-      <Row>
-        <Col span='8'>
-          <div className='grid-content bg-purple'></div>
-        </Col>
-        <Col span='8'>
-          <div className='grid-content bg-purple-light'></div>
-        </Col>
-        <Col span='8'>
-          <div className='grid-content bg-purple'></div>
-        </Col>
-      </Row>
-      <Row>
-        <Col span='6'>
-          <div className='grid-content bg-purple'></div>
-        </Col>
-        <Col span='6'>
-          <div className='grid-content bg-purple-light'></div>
-        </Col>
-        <Col span='6'>
-          <div className='grid-content bg-purple'></div>
-        </Col>
-        <Col span='6'>
-          <div className='grid-content bg-purple-light'></div>
-        </Col>
-      </Row>
-      <Row>
-        <Col span='4'>
-          <div className='grid-content bg-purple'></div>
-        </Col>
-        <Col span='4'>
-          <div className='grid-content bg-purple-light'></div>
-        </Col>
-        <Col span='4'>
-          <div className='grid-content bg-purple'></div>
-        </Col>
-        <Col span='4'>
-          <div className='grid-content bg-purple-light'></div>
-        </Col>
-        <Col span='4'>
-          <div className='grid-content bg-purple'></div>
-        </Col>
-        <Col span='4'>
-          <div className='grid-content bg-purple-light'></div>
-        </Col>
-      </Row> */}
-      {/* <Row gutter='20'>
-        <Col span='6'>
-          <div className='grid-content bg-purple'></div>
-        </Col>
-        <Col span='6'>
-          <div className='grid-content bg-purple'></div>
-        </Col>
-        <Col span='6'>
-          <div className='grid-content bg-purple'></div>
-        </Col>
-        <Col span='6'>
-          <div className='grid-content bg-purple'></div>
-        </Col>
-      </Row> */}
-
 
       <Row gutter='10'>
         <Col xs='8' sm='6' md='4' lg='3' xl='1'>
