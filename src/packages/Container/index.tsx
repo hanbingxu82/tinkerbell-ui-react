bc3bf43a294e430cbb8425db0d6c9d593622a155
韩旭小天才 <905583741@qq.com>
2021-12-30 14:11:18 +0800

    fix:调整页面 Props

--- src/packages/Container/index.tsx
+++ src/packages/Container/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-23 15:58:16
- * @LastEditTime: 2021-12-24 09:17:06
+ * @LastEditTime: 2021-12-24 10:06:18
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /tinkerbell-ui-react/src/packages/Container/index.tsx
@@ -22,7 +22,6 @@ function Container(props: any) {
         setIsHF('vertical')
       }
     })
-    console.log(props)
   }, [])
   return (
     <div
