12b52029c0465c5c4e18babd5b19d44e8b262c89
韩旭小天才 <905583741@qq.com>
2022-01-05 13:58:29 +0800

    feat:调整页面信息

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-13 14:52:54
- * @LastEditTime: 2021-12-23 17:24:16
+ * @LastEditTime: 2022-01-05 10:22:06
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /hxreact/example/src/App.tsx
@@ -12,6 +12,16 @@ import { ExampleComponent, Button, Container } from 'hxreact'
 import 'hxreact/dist/index.css'
 
 const App = () => {
+  let a: any = `[[0, 0, 0, 0, 3, 0, 0, 0, 0, 0], [0, 0, 8, 1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 2, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]`
+  a = a.substr(1).substring(-1, a.length - 3)
+  a = a.split('], ')
+  a = a.map((item: any) => {
+    item += ']'
+    item = item.substr(1).substring(-1, item.length - 2)
+    return item.split(', ')
+  })
+  console.log(typeof undefined)
+  console.log(a)
   const [loading, setLoading] = useState(false)
   return (
     <div>
