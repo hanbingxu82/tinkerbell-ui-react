5081c3a09a63b1a71f403a58b13b57d501aa79f0
韩旭小天才 <905583741@qq.com>
2022-01-06 15:42:48 +0800

    feat:调整页面信息

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-13 14:52:54
- * @LastEditTime: 2022-01-05 10:22:06
+ * @LastEditTime: 2022-01-06 15:42:38
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /hxreact/example/src/App.tsx
@@ -12,6 +12,7 @@ import { ExampleComponent, Button, Container } from 'hxreact'
 import 'hxreact/dist/index.css'
 
 const App = () => {
+  console.log('test')
   let a: any = `[[0, 0, 0, 0, 3, 0, 0, 0, 0, 0], [0, 0, 8, 1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 2, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]`
   a = a.substr(1).substring(-1, a.length - 3)
   a = a.split('], ')
