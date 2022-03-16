1f8cc9382a07e4674d2776e9d5e2d2f76386ad16
韩旭小天才 <905583741@qq.com>
2022-03-16 10:15:38 +0800

    style:调整button type的text line-height属性

--- src/packages/Button/index.tsx
+++ src/packages/Button/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-13 16:17:28
- * @LastEditTime: 2022-03-07 15:51:14
+ * @LastEditTime: 2022-03-16 09:04:49
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /tinkerbell-ui-react/src/packages/Button/index.tsx
