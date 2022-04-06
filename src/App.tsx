d95a913404a392284f2d0c3d6a5826a63a94091f
韩旭小天才 <905583741@qq.com>
2022-04-06 15:47:53 +0800

    fix:调整代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-28 11:17:51
- * @LastEditTime: 2022-04-06 14:15:13
+ * @LastEditTime: 2022-04-06 14:58:29
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /tinkerbell-ui-react/src/App.tsx
@@ -163,7 +163,11 @@ const App = (props: any) => {
   return (
     <div>
       <Popover
-        content={<a href="#/" onClick={hide}>Close</a>}
+        content={
+          <a href='#/' onClick={hide}>
+            Close
+          </a>
+        }
         title='Title'
         trigger='click'
         visible={value}
--- src/packages/Popover/index.tsx
+++ src/packages/Popover/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-06 10:43:31
- * @LastEditTime: 2022-04-06 11:39:44
+ * @LastEditTime: 2022-04-06 14:58:27
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /tinkerbell-ui-react/src/packages/Popover/index.tsx
@@ -38,7 +38,14 @@ const Popover: any = React.forwardRef<any>((props: any, ref) => {
   }
 
   //   const prefixCls = getPrefixCls('popover', customizePrefixCls)
-  return <Tooltip prefixCls='tb-popover' {...otherProps} ref={ref as any} title={getOverlay()} />
+  return (
+    <Tooltip
+      prefixCls='tb-popover'
+      {...otherProps}
+      ref={ref as any}
+      title={getOverlay()}
+    />
+  )
 })
 
 Popover.displayName = 'Popover'
