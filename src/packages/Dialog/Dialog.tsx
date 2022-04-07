71192dfd0f3eb4f57ba7749aee8e381d2dfa1683
韩旭小天才 <905583741@qq.com>
2022-04-07 11:14:45 +0800

    fix:调整 eslint 警告

--- src/packages/Dialog/Dialog.tsx
+++ src/packages/Dialog/Dialog.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-06 16:48:09
- * @LastEditTime: 2022-04-07 10:37:54
+ * @LastEditTime: 2022-04-07 11:14:22
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /tinkerbell-ui-react/src/packages/Dialog/dialog.tsx
@@ -71,7 +71,7 @@ const Dialog: any = React.forwardRef((props: any, wrap: any) => {
         document.body.style.removeProperty('overflow')
       }
     }
-  }, [])
+  }, []) // eslint-disable-line
 
   function onKeyDown(e: any): void {
     const { closeOnPressEscape } = props
