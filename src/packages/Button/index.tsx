b3f52106a65b6a205ccb6a14ba1503d58d41a38f
韩旭小天才 <905583741@qq.com>
2022-04-08 14:05:25 +0800

    fix:调整 Button 组件 文本在不传递图标时有 4px 的 left像素

--- src/packages/Button/index.tsx
+++ src/packages/Button/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-13 16:17:28
- * @LastEditTime: 2022-03-28 11:27:43
+ * @LastEditTime: 2022-04-08 14:04:15
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /tinkerbell-ui-react/src/packages/Button/index.tsx
@@ -32,7 +32,7 @@ interface Iprops {
 const Button = (props: any) => {
   const {
     type = 'default',
-    icon = ' ',
+    icon = '',
     iconStyle,
     disabled,
     loading,
