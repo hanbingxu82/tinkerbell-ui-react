30281aea9fccbf745ff36571a9c951bca3560018
韩旭小天才 <905583741@qq.com>
2022-04-15 14:01:20 +0800

    fix:调整 Select 右侧图标  上下互换

--- src/packages/Select/Select.tsx
+++ src/packages/Select/Select.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-12 15:37:35
- * @LastEditTime: 2022-04-15 09:32:32
+ * @LastEditTime: 2022-04-15 13:51:20
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /tinkerbell-ui-react/src/packages/Select/Select.tsx
@@ -537,7 +537,7 @@ const Select: any = React.forwardRef((props: any, _ref: any) => {
       ? 'icon-close'
       : props.remote && props.filterable
       ? ''
-      : `icon-arrow-down ${state.visible ? 'is-reverse' : ''}`
+      : `icon-arrow-up ${state.visible ? 'is-reverse' : ''}`
   }
 
   function showCloseIcon(): boolean {
