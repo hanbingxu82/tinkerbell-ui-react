c206636b47f80495ab48dc22fd111b6fe4a91f64
韩旭小天才 <905583741@qq.com>
2022-06-30 16:31:29 +0800

    fix:调整 Button 组件注释

--- src/packages/Button/index.tsx
+++ src/packages/Button/index.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-12-13 16:17:28
- * @LastEditTime: 2022-05-24 16:13:31
- * @LastEditors: 韩旭小天才 905583741@qq.com
+ * @LastEditTime: 2022-06-29 11:03:17
+ * @LastEditors: 韩旭小天才
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /tinkerbell-ui-react/src/packages/Button/index.tsx
  */
@@ -11,8 +11,10 @@
 import React from 'react'
 import Icon from '../Icon'
 import './index.scss'
-var tinycolor = require('tinycolor2')
-const classnames = require('classnames')
+var tinycolor = require('tinycolor2') // 颜色转换插件
+const classnames = require('classnames') // classname 转换插件
+
+// interface接口
 interface Iprops {
   type: string
   disabled: boolean
@@ -28,34 +30,35 @@ interface Iprops {
   iconStyle: any
   icon: string
   textColor: string
-  animationType: string
 }
 
 const Button = (props: any) => {
   const {
-    type = 'default',
-    icon = '',
-    iconStyle,
-    disabled,
-    loading,
-    nativeType = 'button',
-    size = 'default',
-    plain,
-    round,
-    dashed,
-    transparent,
-    background,
-    loadingIcon,
-    textColor,
-    ...restprop
+    type = 'default', // 样式类型
+    icon = '', // 图标样式
+    iconStyle, // icon 图标style
+    disabled, // 禁用效果
+    loading, // loading 加载效果
+    nativeType = 'button', // button 原生属性的type类型
+    size = 'default', // size 大小效果
+    plain, // 镂空 style 效果
+    round, // 圆角 style 效果
+    dashed, // 虚线 style 效果
+    transparent, // 是否为透明按钮
+    background, // 默认按钮是否带有拟物背景
+    loadingIcon, // 加载效果图标样式
+    textColor, // 文本颜色
+    ...restprop //其余所有传递给 原生 button 的方法 属性
   }: Iprops = props
 
+  // icon图标style
   function iconStyles() {
     return {
       ...textStyle(),
       ...iconStyle
     }
   }
+  // 默认 文本样式给与的颜色
   function colorMap() {
     return {
       primary: '#1089ff',
@@ -65,19 +68,23 @@ const Button = (props: any) => {
       danger: '#ff4d4f'
     }
   }
-
+  // 点击事件触发父行为
   function handleClick(evt: any) {
     props.onClick && props.onClick(evt)
   }
+  // 鼠标进入触发父行为
   function handMouseEnter(evt: any) {
     props.onMouseEnter && props.onMouseEnter(evt)
   }
+  // 鼠标离开触发父行为
   function handMouseLeave(evt: any) {
     props.onMouseLeave && props.onMouseLeave(evt)
   }
+  // 触发获取焦点事件
   function handFocus(evt: any) {
     props.onFocus && props.onFocus(evt)
   }
+  // text 文本颜色
   function textStyle() {
     let color = textColor
       ? colorMap()[textColor]
@@ -91,6 +98,7 @@ const Button = (props: any) => {
     }
     return {}
   }
+  // 判断传递的组件是否为 文本 按钮 如果不是
   if (type !== 'text') {
     return (
       <button
@@ -120,6 +128,7 @@ const Button = (props: any) => {
           props.className
         }
       >
+        {/* 判断有没有loading加载 */}
         {loading && (
           <span className={'btn-icon-loading'}>
             <Icon
@@ -129,6 +138,7 @@ const Button = (props: any) => {
             />
           </span>
         )}
+        {/* 判断有没有静态图标 */}
         {icon && !loading && (
           <i className={['iconfont', icon].join(' ')} style={iconStyles()}></i>
         )}
@@ -136,6 +146,7 @@ const Button = (props: any) => {
       </button>
     )
   } else {
+    // 如果是文本按钮
     return (
       <button
         {...restprop}
@@ -155,6 +166,7 @@ const Button = (props: any) => {
           props.className
         }
       >
+        {/* 判断图标 在这里就没必要给加载判断了 */}
         {icon && !loading && (
           <i className={['iconfont', icon].join(' ')} style={iconStyles()}></i>
         )}
