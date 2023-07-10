e5246a6c2fe99f07512aa17fa79fc3a3a12c9f0f
韩炳旭 <905583741@qq.com>
2023-07-10 17:45:58 +0800

    fix:调整 dialog 弹窗close

--- src/packages/Dialog/Dialog.tsx
+++ src/packages/Dialog/Dialog.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-06 16:48:09
- * @LastEditTime: 2023-07-10 17:37:28
+ * @LastEditTime: 2023-07-10 17:44:24
  * @LastEditors: hanbingxu
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /tinkerbell-ui-react/src/packages/Dialog/dialog.tsx
@@ -189,7 +189,7 @@ const Dialog: any = React.forwardRef((props: any, wrap: any) => {
                     className='tb-dialog__headerbtn'
                     onClick={(e) => close(e)}
                   >
-                    <i className='tb-dialog__close tb-icon tb-icon-close' />
+                    <i className='tb-dialog__close iconfont icon-close' />
                   </button>
                 )}
               </div>
